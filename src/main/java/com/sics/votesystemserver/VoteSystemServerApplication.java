b6acb34d3cd51496594e0bbce0479d7339a9e9c5
Liangjc <443808626@qq.com>
2022-08-05 17:03:55 +0800

    feat：create project

---
+++ src/main/java/com/sics/votesystemserver/VoteSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.sics.votesystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSystemServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VoteSystemServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sics/votesystemserver/VoteSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sics.votesystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSystemServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
